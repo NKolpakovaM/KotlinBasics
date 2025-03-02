b6e9f1433395cdd7abb9d199cc861d5f6a943a5e
NKolpakovaM <KolpakovaNM@yandex.ru>
2025-03-02 23:55:20 +0500

    KS-5-1 задача на авторизацию

---
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -0,0 +1,9 @@
+fun main () {
+    println("Введите сумму чисел: 236 и 3")
+    var input = readln().toIntOrNull()
+    if (input == 239) {
+        println("Добро пожаловать!")
+    } else {
+        println("Доступ запрещен.")
+    }
+}
