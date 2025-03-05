7c1336db49d547c777e66f500387166213cb2950
NKolpakovaM <KolpakovaNM@yandex.ru>
2025-03-05 14:03:43 +0500

    KS-4-2 код исправлен

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -1,21 +1,13 @@
-fun main () {
+const val LOWER_WEIGHT = 35
+const val UPPER_WEIGHT = 100
+const val AVERAGE_VOLUME = 100
+
+fun main() {
     var weight = 20
     var volume = 80
-    val comparisonResult1: Boolean = (lowerWeight <= weight) && (weight <= upperWeight) && (volume >= averageVolume)
-    if (comparisonResult1 == true) {
-        println("Это средний груз")
-    } else {
-        println("Это не средний груз")
-    }
+    println("Это ${if (weight >= LOWER_WEIGHT && weight <= UPPER_WEIGHT && volume >= AVERAGE_VOLUME) "средний груз" else "не средний груз"}")
+
     weight = 50
     volume = 100
-    val comparisonResult2: Boolean = (lowerWeight <= weight) && (weight <= upperWeight) && (volume >= averageVolume)
-    if (comparisonResult2 == true) {
-        println("Это средний груз")
-    } else {
-        println("Это не средний груз")
-    }
+    println("Это ${if (weight >= LOWER_WEIGHT && weight <= UPPER_WEIGHT && volume >= AVERAGE_VOLUME) "средний груз" else "не средний груз"}")
 }
-const val lowerWeight = 35
-const val upperWeight = 100
-const val averageVolume = 100
