b7118ca29c34bbfb83eee9a63b705822159ab6fb
NKolpakovaM <KolpakovaNM@yandex.ru>
2025-03-05 15:09:19 +0500

    KS-5-3 задача по выигрышным числам решена

---
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -0,0 +1,22 @@
+fun main() {
+    println("Угадайте два числа от 0 до 42 для участия в лотерее.")
+    println("Введите первое число:")
+    val input1 = readln().toIntOrNull()
+    println("Введите второе число:")
+    val input2 = readln().toIntOrNull()
+
+    if (input1 == null || input2 == null) {
+        println("Некорректный ввод.")
+        return
+    }
+
+    val correctGuesses = (input1 == RIGHT_NUMBER1 || input1 == RIGHT_NUMBER2) + (input2 == RIGHT_NUMBER1 || input2 == RIGHT_NUMBER2)
+
+    when (correctGuesses) {
+        2 -> println("Поздравляем! Вы выиграли главный приз!")
+        1 -> println("Вы выиграли утешительный приз!")
+        else -> println("Неудача! Выигрышные числа: $RIGHT_NUMBER1 и $RIGHT_NUMBER2")
+    }
+}
+val RIGHT_NUMBER1 = 3
+val RIGHT_NUMBER2 = 7
