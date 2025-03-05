ef3b7b86d85667c37b71ce1349c194f5668a1a58
NKolpakovaM <KolpakovaNM@yandex.ru>
2025-03-05 14:17:09 +0500

    KS-4-3 код исправлен

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,8 +1,11 @@
-fun main () {
+fun main() {
     val sunnyWeather: Boolean = true
     val tentAvailable: Boolean = true
     val humidity: Int = 20
     val season: String = "winter"
-    val isGoodForGrowth: Boolean = sunnyWeather && tentAvailable && (humidity == 20) && !(season == "winter")
+
+    val FAVORABLE_HUMIDITY = 20 // добавлена константа
+    val UNFAVORABLE_SEASON = "winter" // добавлена константа
+    val isGoodForGrowth: Boolean = sunnyWeather && tentAvailable && (humidity == FAVORABLE_HUMIDITY) && (season != UNFAVORABLE_SEASON)
     println("Благоприятные ли условия для роста бобовых? Ответ: $isGoodForGrowth")
 }
