dd1b0caefc5d1bed9523cc6d8a6769525b4d8dd3
NKolpakovaM <KolpakovaNM@yandex.ru>
2025-03-06 09:33:02 +0500

    KS-5-1 задача исправлена расчету суммы

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -1,7 +1,10 @@
 fun main () {
+    var number1 = 236
+    var number2 = 3
+    val sum = number1 + number2
     println("Введите сумму чисел: 236 и 3")
     var input = readln().toIntOrNull()
-    if (input == 239) {
+    if (input == sum) {
         println("Добро пожаловать!")
     } else {
         println("Доступ запрещен.")
