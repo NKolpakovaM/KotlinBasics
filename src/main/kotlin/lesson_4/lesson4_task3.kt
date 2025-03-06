e6a9b1e42110112e745d130a298d20cf0c6cfce6
NKolpakovaM <KolpakovaNM@yandex.ru>
2025-03-06 09:23:57 +0500

    KS-4-3 задача исправлена по константам и ==

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,11 +1,14 @@
+const val WEATHER_GOOD: Boolean = true
+const val TENT_YES: Boolean = true
+const val FAVORABLE_HUMIDITY = 20
+const val UNFAVORABLE_SEASON = "winter"
+
 fun main() {
     val sunnyWeather: Boolean = true
     val tentAvailable: Boolean = true
     val humidity: Int = 20
     val season: String = "winter"
 
-    val FAVORABLE_HUMIDITY = 20 // добавлена константа
-    val UNFAVORABLE_SEASON = "winter" // добавлена константа
-    val isGoodForGrowth: Boolean = sunnyWeather && tentAvailable && (humidity == FAVORABLE_HUMIDITY) && (season != UNFAVORABLE_SEASON)
+    val isGoodForGrowth: Boolean = sunnyWeather == WEATHER_GOOD && tentAvailable == TENT_YES && humidity == FAVORABLE_HUMIDITY && season != UNFAVORABLE_SEASON
     println("Благоприятные ли условия для роста бобовых? Ответ: $isGoodForGrowth")
 }
