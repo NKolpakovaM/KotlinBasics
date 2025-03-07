8629a5201c3d6c198b9e719ad051a9c9662e2a46
NKolpakovaM <KolpakovaNM@yandex.ru>
2025-03-07 09:40:48 +0500

    KS-5-1 пробелы убраны

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -2,7 +2,7 @@ fun main () {
     var number1 = 236
     var number2 = 3
     val sum = number1 + number2
-    println("Введите сумму чисел: 236 и 3")
+    println("Введите сумму чисел: number 1 и number 2")
     var input = readln().toIntOrNull()
     if (input == sum) {
         println("Добро пожаловать!")
