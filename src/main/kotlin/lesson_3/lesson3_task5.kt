4222e50d1a935a5c4cf9805427ef8ec401f26f42
NKolpakovaM <KolpakovaNM@yandex.ru>
2025-03-10 11:45:58 +0500

    KS-3-5 задача по шахматам решена

---
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -0,0 +1,10 @@
+fun main () {
+    var firstPosition = "D2"
+    var lastPosition = "D4"
+    var codeNumber = 0
+    var output = println("Данные текущего хода [откуда - куда; номер хода]: $firstPosition - $lastPosition; $codeNumber")
+    print("Данные текущего хода [откуда - куда; номер хода]: ")
+    print("$firstPosition - ")
+    print("$lastPosition;")
+    print (" $codeNumber")
+}
